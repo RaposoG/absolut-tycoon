b9fe42a7024fe399545083848ccb2d5d1f68b893
raposoG <gabriel.henrique7@hotmail.com>
2025-05-03 20:57:16 -0300

    feat: add theme provider and toggle functionality
    - Implemented ThemeProvider to manage theme state using next-themes.
    - Added ModeToggle component for switching between light, dark, and system themes.
    - Created DropdownMenu component for theme selection UI.
    - Updated RootLayout to include ThemeProvider for consistent theming across the app.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 export const metadata: Metadata = {
   title: "Absolut Tycoon",
@@ -12,8 +13,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
