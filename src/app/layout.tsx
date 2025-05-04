0356fd95f744891475c26c6e9a02f0f732f12ced
raposoG <gabriel.henrique7@hotmail.com>
2025-05-04 11:21:32 -0300

    fix(layout): corrigir idioma da tag HTML
    - Alterado o atributo 'lang' da tag <html> de "en" para "pt-br" no arquivo layout.tsx.
    
    Essa mudança garante que o idioma correto seja utilizado, melhorando a acessibilidade e a experiência do usuário para falantes de português.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="pt-br" suppressHydrationWarning>
       <body className={`antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
